370cf3d59d2bf3fecce8f1d8654171efff6fff29
Zephira <999llamaz@gmail.com>
2022-09-11 00:10:41 -0500

    Workspace setup

---
+++ src/main/java/net/zephira/pogtweaks/PogTweaks.java
@@ -0,0 +1,14 @@
+package net.zephira.pogtweaks;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class PogTweaks implements ModInitializer {
+	private static final String MOD_ID = "pogtweaks";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
---
+++ src/main/java/net/zephira/pogtweaks/PogTweaksClient.java
@@ -0,0 +1,12 @@
+package net.zephira.pogtweaks;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class PogTweaksClient implements ClientModInitializer{
+
+    @Override
+    public void onInitializeClient() {
+        
+    }
+    
+}
